bb0461d0a7f6334dc62a4800d7e765f2a8dde887
Anshul Singh <anshulsingh024@yahoo.com>
2025-06-17 13:02:11 +0530

    Added AsyncHandler, updated promises for connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 // import mongoose from "mongoose"; //DB Se connect krega
 import dotenv from 'dotenv'
 import connectDB from "./db/dbConnect.js";
+import { app } from './app.js';
 
 dotenv.config({path:'./.env'})
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error Occured in Express App ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Express App Listening on PORT : ${process.env.PORT}`)
+    })
+})
+.catch( (e) => {
+    console.error("DB Connection Error:", e);
+    process.exit(1);
+});
+
