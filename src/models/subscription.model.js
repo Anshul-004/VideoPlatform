93be7f42af9f4702bc845eee03a2934a8e9d045c
Anshul Singh <anshulsingh024@yahoo.com>
2025-07-08 20:33:40 +0530

    Updated User Controller

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const subscriptionSchema = new mongoose.Schema({
-    subscriber:
+    subscriber: // subscribed count for a user (What channels a user has subscribed)
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User"
         },
 
-    channel:{
+    channel:{ //subscriber count for a channel
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"        
     }
